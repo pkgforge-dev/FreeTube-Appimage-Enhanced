2d086b922263b2a1a60c05d5e69b75e9adcccf6c
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-05 21:26:19 +0200

    Fix StartupWMClass on Wayland
    Wayland is sensitive to cases in `wmclass`, while X11 is not, so `freetube` works on X11 also.

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
+export STARTUPWMCLASS=freetube
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
